78b9467745a7aa86d03d051dc17ac6f572a1d116
Luciano <dipasquale.luc@hotmail.com>
2020-10-21 21:42:18 -0300

    agregamos el login

---
+++ ServiHogar/www/mvc/controlador/login.js
@@ -0,0 +1,34 @@
+$('#Nuevo').click(Nuevo);
+ function Nuevo(){
+ window.location.href='../vista/crearusuario - copia.html';
+ }
+
+ 
+$('#Ingresar').click(Ingresar);
+ function Ingresar(){
+ //var inUsuario = $('#inUsuario');
+ //var usuario = inUsuario.val().trim();
+	//inUsuario.val(usuario);
+ //var inContrasena = $('#inContrasena');
+ //var contrasena = inContrasena.val().trim();
+	//inContrasena.val(contrasena);
+	
+ //var res_validar_ingreso = "2";//validar_ingreso(usuario, contrasena);
+
+ 
+ 
+// if (res_validar_ingreso == "1") {
+	//alert ("Ingreso incorrecto");
+	//} else {
+		//if (res_validar_ingreso == "2") {
+			window.location.href='../vista/principal.html';
+		//} else {
+			//if (res_validar_ingreso == "3") {
+				//alert ("Ingreso correcto!!!");
+			//} else {
+				//alert ("ERROR!!!!");
+					//}
+			//}
+		//}
+
+	}
