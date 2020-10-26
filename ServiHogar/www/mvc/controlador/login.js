12cc5f8ada2a2f5c8f832a3b606615fbb8ec89e9
Federico Diaz <federico.dario.diaz@gmail.com>
2020-10-25 22:00:21 -0300

    modelo, dao/dto y login sin funcionar

--- ServiHogar/www/mvc/controlador/login.js
+++ ServiHogar/www/mvc/controlador/login.js
@@ -3,33 +3,22 @@ $('#Nuevo').click(Nuevo);
  window.location.href='../vista/crearusuario - copia.html';
  }
 
- 
 $('#Ingresar').click(Ingresar);
- function Ingresar(){
- window.location.href='../vista/principal.html';
- //var inUsuario = $('#inUsuario');
- //var usuario = inUsuario.val().trim();
-	//inUsuario.val(usuario);
- //var inContrasena = $('#inContrasena');
- //var contrasena = inContrasena.val().trim();
-	//inContrasena.val(contrasena);
-	
- //var res_validar_ingreso = "2";//validar_ingreso(usuario, contrasena);
-
- 
- 
-// if (res_validar_ingreso == "1") {
-	//alert ("Ingreso incorrecto");
-	//} else {
-		//if (res_validar_ingreso == "2") {
-			
-		//} else {
-			//if (res_validar_ingreso == "3") {
-				//alert ("Ingreso correcto!!!");
-			//} else {
-				//alert ("ERROR!!!!");
-					//}
-			//}
-		//}
-
+function Ingresar(){
+	var inUsername = $('#Username');
+	var username = inUsername;//.val().trim();
+	inUsername.val(username);
+	var inPassword = $('#Password');
+	var password = inPassword;//.val().trim();
+	inPassword.val(password);
+	var res_validar_ingreso = validar_ingreso(username, password);
+	if (res_validar_ingreso == "1") {
+	alert ("Ingreso incorrecto");
+	} else {
+		if (res_validar_ingreso == "2") {
+			window.location.href='../vista/principal.html';
+		} else {
+			alert ("ERROR!!!!");
+		}
 	}
+}
