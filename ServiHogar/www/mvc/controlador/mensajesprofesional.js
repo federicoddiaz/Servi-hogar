9795f95b16d87d28e68decf18b91c223cc1f8784
NicolasLlanos8799 <54457809+NicolasLlanos8799@users.noreply.github.com>
2020-10-27 18:41:02 -0300

    Se creo todas las pantallas para el profesional

--- ServiHogar/www/mvc/controlador/mensajesprofesional.js
+++ ServiHogar/www/mvc/controlador/mensajesprofesional.js
@@ -8,20 +8,20 @@ document.getElementById('pcimagenes').align="center";
  
  $('#inicio').click(inicio);
  function inicio(){
- window.location.href='../vista/principal.html';
+ window.location.href='../vista/principalprofesional.html';
  }
  
  $('#perfil').click(perfil);
  function perfil(){
- window.location.href='../vista/principalperfil.html';
+ window.location.href='../vista/principalperfilprofesional.html';
  }
  
  $('#mensajes').click(mensajes);
  function mensajes(){
- window.location.href='../vista/mensajes.html';
+ window.location.href='../vista/mensajesprofesional.html';
  }
  
  $('#configuraciones').click(configuraciones);
  function configuraciones(){
- window.location.href='../vista/configuraciones.html';
+ window.location.href='../vista/configuracionesprofesional.html';
  }
